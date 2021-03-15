2a2a2683fcf795432756e6d471e5e03d8af90505
eakyel <e.akyel@outlook.com>
2021-03-15 17:38:54 +0300

    Select DOM elements

--- app/script/app.js
+++ app/script/app.js
@@ -1,1 +1,12 @@
 "use strict";
+// Select DOM Elements
+var score0El = document.querySelector("#score--0");
+var score1El = document.querySelector("#score--1");
+var current0El = document.querySelector("#current--0");
+var current1El = document.querySelector("#current--1");
+var player0El = document.querySelector(".player--0");
+var player1El = document.querySelector(".player--1");
+var diceEl = document.querySelector(".dice");
+var btnNew = document.querySelector(".btn--new");
+var btnRoll = document.querySelector(".btn--roll");
+var btnHold = document.querySelector(".btn--hold");
---
+++ src/script/app.ts
@@ -0,0 +1,13 @@
+// Select DOM Elements
+const score0El = document.querySelector("#score--0") as HTMLTextAreaElement;
+const score1El = document.querySelector("#score--1") as HTMLTextAreaElement;
+const current0El = document.querySelector("#current--0") as HTMLTextAreaElement;
+const current1El = document.querySelector("#current--1") as HTMLTextAreaElement;
+
+const player0El = document.querySelector(".player--0") as HTMLElement;
+const player1El = document.querySelector(".player--1") as HTMLElement;
+
+const diceEl = document.querySelector(".dice") as HTMLImageElement;
+const btnNew = document.querySelector(".btn--new") as HTMLButtonElement;
+const btnRoll = document.querySelector(".btn--roll") as HTMLButtonElement;
+const btnHold = document.querySelector(".btn--hold") as HTMLButtonElement;
