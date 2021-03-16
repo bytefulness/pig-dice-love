9c1a86baee3eca7265bc475d46b49e5fef49632a
eakyel <e.akyel@outlook.com>
2021-03-16 14:25:37 +0300

    Add dice roll functionality
    This commit generates random dice value between 1-6 and creates dice image in UI based on generated dice value.

--- app/script/app.js
+++ app/script/app.js
@@ -10,3 +10,48 @@ var diceEl = document.querySelector(".dice");
 var btnNew = document.querySelector(".btn--new");
 var btnRoll = document.querySelector(".btn--roll");
 var btnHold = document.querySelector(".btn--hold");
+// APP Class
+var App = /** @class */ (function () {
+    function App() {
+        this.init();
+        // #Event Listeners
+        btnNew.addEventListener("click", this.init.bind(this));
+        btnRoll.addEventListener("click", this.rollDice.bind(this));
+    }
+    App.prototype.init = function () {
+        this.scores = [0, 0];
+        this.currentScore = 0;
+        this.activePlayer = 0;
+        this.playing = true;
+        // #Set initial and reset UI
+        // ##Big Scores
+        score0El.textContent = "0";
+        score1El.textContent = "0";
+        // ##Current Scores
+        current0El.textContent = "0";
+        current1El.textContent = "0";
+        diceEl.classList.add("hidden");
+        // #Remove winner style class
+        player0El.classList.remove("player--winner");
+        player1El.classList.remove("player--winner");
+        player0El.classList.add("player--active");
+        player1El.classList.remove("player--active");
+    };
+    App.prototype.rollDice = function () {
+        if (this.playing) {
+            // #Generate random dice roll
+            var dice = Math.floor(Math.random() * 6) + 1;
+            // #Display dice
+            diceEl.classList.remove("hidden");
+            diceEl.src = "/images/dice-" + dice + ".png";
+            // #Check for rolled 1
+            if (dice !== 1) {
+                // Add dice value to current score
+                this.currentScore += dice;
+                document.querySelector("#current--" + this.activePlayer).textContent = "" + this.currentScore;
+            }
+        }
+    };
+    return App;
+}());
+var app = new App();
--- src/script/app.ts
+++ src/script/app.ts
@@ -11,3 +11,66 @@ const diceEl = document.querySelector(".dice") as HTMLImageElement;
 const btnNew = document.querySelector(".btn--new") as HTMLButtonElement;
 const btnRoll = document.querySelector(".btn--roll") as HTMLButtonElement;
 const btnHold = document.querySelector(".btn--hold") as HTMLButtonElement;
+
+// APP Class
+class App {
+  scores!: number[];
+  currentScore!: number;
+  activePlayer!: number;
+  playing!: Boolean;
+
+  constructor() {
+    this.init();
+
+    // #Event Listeners
+    btnNew.addEventListener("click", this.init.bind(this));
+    btnRoll.addEventListener("click", this.rollDice.bind(this));
+  }
+
+  private init() {
+    this.scores = [0, 0];
+    this.currentScore = 0;
+    this.activePlayer = 0;
+    this.playing = true;
+
+    // #Set initial and reset UI
+
+    // ##Big Scores
+    score0El.textContent = "0";
+    score1El.textContent = "0";
+
+    // ##Current Scores
+    current0El.textContent = "0";
+    current1El.textContent = "0";
+
+    diceEl.classList.add("hidden");
+
+    // #Remove winner style class
+    player0El.classList.remove("player--winner");
+    player1El.classList.remove("player--winner");
+    player0El.classList.add("player--active");
+    player1El.classList.remove("player--active");
+  }
+
+  private rollDice() {
+    if (this.playing) {
+      // #Generate random dice roll
+      const dice: number = Math.floor(Math.random() * 6) + 1;
+
+      // #Display dice
+      diceEl.classList.remove("hidden");
+      diceEl.src = `/images/dice-${dice}.png`;
+
+      // #Check for rolled 1
+      if (dice !== 1) {
+        // Add dice value to current score
+        this.currentScore += dice;
+        document.querySelector(
+          `#current--${this.activePlayer}`
+        )!.textContent = `${this.currentScore}`;
+      }
+    }
+  }
+}
+
+const app = new App();
